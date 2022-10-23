2f2e758feb95988bd7d37861584aec4886edf4b5
Henrique <henrique.macedo@etools.com.br>
2022-10-23 01:49:52 -0300

    header sidebar parte

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
-function App() {
-  return (
-   <h1>hello world</h1>
-  )
-}
+import { Header } from "./Components/Header"
+import { Post } from "./Post"
 
-export default App
+import './global.css'
+
+import styles from './App.module.css'
+import { Sidebar } from "./Components/Sidebar"
+
+export function App() {
+    return (
+        <div>
+            <Header/>
+            <div className={styles.wrapper}>
+                <Sidebar/>
+                <main>
+                    <Post 
+                        author="Henrique" 
+                        content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illo pariatur excepturi, impedit eveniet exercitationem placeat repellendus, voluptates consequuntur voluptate, officia ex reiciendis voluptatum. Saepe pariatur atque asperiores corrupti laborum temporibus!"
+                    />
+                    <Post 
+                        author="Teste" 
+                        content="BORA DOIDO"
+                    />
+                </main>
+            </div>
+        </div>
+    )
+}
